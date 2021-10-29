12d7089b91fdcdc5d656a2bda17bf3f8efa48eec
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-10-29 23:50:24 +0300

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -2,12 +2,5 @@ from pathlib import Path
 
 class Site:
     def __init__(self,source,dest):
-        source=Path(self.source) 
-        dest=Path(self.dest) 
-
-
-    def create_dir(self,path):
-        directory      =    self.dest / relative_to(self.source)
-
-
-        
+        self.source =   Path(source)
+        self.dest   =   Path(dest)
