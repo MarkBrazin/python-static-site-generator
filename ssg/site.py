00f39763acc0f00603764f5147222210e6032d96
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-10-30 00:01:42 +0300

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,4 @@
+from os import mkdir
 from pathlib import Path
 
 class Site:
@@ -7,3 +8,6 @@ class Site:
 
     def create_dir(self, path):
         directory   =   self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True,exist_ok=True)
+    def build(self):
+        self.dest.mkdir(parents=True,exist_ok=True)
