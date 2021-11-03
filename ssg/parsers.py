e8d0ba9066ac6491509bbf0aa77601399506bdb0
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-11-03 22:49:32 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -15,7 +15,7 @@ class Parser:
     
     def read(self,path):
         with open(path,'r') as file:
-            return file.read
+            return file.read()
     
     def write(self,path,dest,content,ext =   ".html"):
         
