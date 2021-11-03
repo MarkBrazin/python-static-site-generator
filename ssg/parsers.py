8cde4c6e9b9862124318c6a245d40312c0bd5de9
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-11-03 22:15:24 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,8 @@
 from os import extsep, read, write
 from typing import List
 from pathlib import Path
+import shutil
+
 
 class Parser:
 
@@ -21,3 +23,5 @@ class Parser:
         with open(full_path,'w') as file:
             file.write(content)
 
+    def copy(self,path,source,dest):
+        shutil.copy2(path,dest / path.relative_to(source)   )
