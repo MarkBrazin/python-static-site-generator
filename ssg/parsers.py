dfd888264f342ea65dab103687917345cebef4ac
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-11-03 21:47:23 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,3 +1,4 @@
+from os import read
 from typing import List
 from pathlib import Path
 
@@ -5,4 +6,10 @@ class Parser:
     extensions: List[str]  =  []
     def valid_extension(self,extension):
         return extension in self.extensions
+    
+    
+    def read(path):
+        with open(path) as file:
+            return read(file)
+
         
