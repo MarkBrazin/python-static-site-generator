cb2924df80fa79f80b8329a8c85d42a459a4fc3d
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-11-03 21:52:47 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,9 +7,11 @@ class Parser:
     def valid_extension(self,extension):
         return extension in self.extensions
     
+    def valid_extensions (self,extensions):
+        return extensions in self.extensions
     
     def read(self,path):
         with open(path,'r') as file:
-            return self.read(file)
+            return read(file)
 
         
