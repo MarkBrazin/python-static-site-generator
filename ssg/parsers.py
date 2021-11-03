fa89c7ed8481882499f5a96189a4e3dcf9cc9a52
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-11-03 21:42:49 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 class Parser:
     extensions: List[str]  =  []
+    def valid_extension(self,extension):
+        if extension in self.extensions:
+            return True
+        else:
+            return False
