725a0987f25176590f8d3e334d61c54e4b6ecfd7
MarkBrazin <92234951+MarkBrazin@users.noreply.github.com>
2021-11-03 22:25:13 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -25,8 +25,8 @@ class Parser:
 
     def copy(self,path,source,dest):
         shutil.copy2(path,dest / path.relative_to(source)   )
-class ResourceParser:
-     extensions  =   [".jpg",".png",".gif",".css","html"]
+    class ResourceParser:
+     extensions  =   [".jpg",".png",".gif",".css",".html"]
     def parse(self, path, source, dest):
          shutil.copy(path,source,dest)
          
